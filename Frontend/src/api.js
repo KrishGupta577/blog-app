ffd4e52000a979d4afe8871d2efddf305242de5b
Krishna Gupta <krishgupta577@gmail.com>
2025-10-25 03:15:39 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://blog-app-pz71-backend.vercel.app/api',
   withCredentials: true,
 });
 
